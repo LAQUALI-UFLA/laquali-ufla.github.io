f63dfa40f51ae3069991e2c029e02f30c0fbf315
Prof Cleiton Nunes <132004620+cleitonqmc@users.noreply.github.com>
2026-09-16 18:56:10 -0300

    Add files via upload

---
+++ maRS/install.R
@@ -0,0 +1,4 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+if (!require("remotes", quietly = TRUE)) install.packages("remotes")
+remotes::install_url("https://laquali-ufla.github.io/maRS_inst_pkg.zip",upgrade = "always")
+library(maRS)
